28a2ddbc8f3ef199e48b514480f0440f995dae59
mikayla-stokes <mikaylastokes@usf.edu>
2024-07-18 23:32:52 -0400

    added u number

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//U99796912
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Gallery from './components/Gallery';
